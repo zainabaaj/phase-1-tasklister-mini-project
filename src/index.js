8fe32e5293bbba17d574edfa417025c98e2feb84
Ben Cavins <ben.cavins@flatironschool.com>
2023-05-03 14:56:41 -0400

    do lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form')
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const taskTxt = event.target["new-task-description"].value
+
+    const list = document.querySelector('#tasks')
+    const newLi = document.createElement('li')
+    newLi.textContent = taskTxt
+
+    list.appendChild(newLi)
+  })
 });
